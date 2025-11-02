2c015dfea1359c866c0e9d85040d00d9237ba311
mighantos <michalsvag@gmail.com>
2025-11-03 00:21:27 +0100

    feat(meetings): add bruno files

---
+++ src/main/kotlin/com/mighantos/partner_film_chooser/repository/MeetingInstanceRepository.kt
@@ -0,0 +1,12 @@
+package com.mighantos.partner_film_chooser.repository
+
+import com.mighantos.partner_film_chooser.model.MeetingInstance
+import com.mighantos.partner_film_chooser.model.MeetingPlan
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+interface MeetingInstanceRepository : JpaRepository<MeetingInstance, UUID> {
+    fun findFirstByMeetingPlanOrderByDate(meetingPlan: MeetingPlan): Optional<MeetingInstance>
+}
